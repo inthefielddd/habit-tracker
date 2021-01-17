1bcafc928752fef7eb3f52106afbc1c8469930f2
hyeon ji <inthefield1027@gmail.com>
2021-01-17 20:18:07 +0900

    add function component and memo

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,33 +1,35 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends PureComponent {
-    formRef = React.createRef();
-    inputRef = React.createRef();
-
-    onSubmit = (e) =>{
-        e.preventDefault();
-        const name = this.inputRef.current.value
-        name && this.props.onAdd(name);
-        // this.inputRef.current.value = "";
-        //정석은 formRef를 추가해서
-        this.formRef.current.reset()
-    }
-    render() {
-        console.log('add form')
+const HabitAddForm = memo(
+    (props) => {
+        const formRef = React.createRef();
+        const inputRef = React.createRef();
+    
+         const onSubmit = (e) =>{
+            e.preventDefault();
+            const name = inputRef.current.value
+            name && props.onAdd(name);
+            // this.inputRef.current.value = "";
+            //정석은 formRef를 추가해서
+            formRef.current.reset()
+        };
         return (
-                <form 
-                ref={this.formRef}
-                className="add-form"
-                 onSubmit={this.onSubmit}>
-                <input 
-                ref={this.inputRef}
-                type="text"
-                className="add-input"
-                placeholder="Habit"/>
-                <button className="add-btn">Add</button>
-            </form>
-        );
-    }
-}
+            <form 
+            ref={formRef}
+            className="add-form"
+             onSubmit={onSubmit}>
+            <input 
+            ref={inputRef}
+            type="text"
+            className="add-input"
+            placeholder="Habit"/>
+            <button className="add-btn">Add</button>
+        </form>
+    );
+});
+            
+    
 
 export default HabitAddForm;
+
+
